5b204901e2b6fda5920f9eefb56d6c91a2dfeb46
Tushaar Gangavarapu <tushaargvsg45@gmail.com>
2024-05-17 11:58:56 -0400

    Update latest changes

--- linear_rnn/reference/scan.py
+++ linear_rnn/reference/scan.py
@@ -1,11 +1,30 @@
 import torch
+from einops import rearrange
 
 
 def scan_diag_a_ref(x: torch.Tensor, a: torch.Tensor):
-    """(Sequential) scan reference."""
-    # x, a: (batch, seq_len, dim).
+    """(Sequential) scan reference with diagonal A."""
+    # x, a: (batch, seq_len, dim)
     # h_t: (batch, dim)
     h_t = torch.zeros_like(x[:, 0])
-    for t in range(1, x.shape[1]):
+    for t in range(x.shape[1]):
         h_t = a[:, t] * h_t + x[:, t]
+    return h_t
+
+
+def scan_block_diag_a_ref(x: torch.Tensor, a: torch.Tensor):
+    """(Sequential) scan reference with block-diagonal A."""
+    # x: (batch, seq_len, dim)
+    # a: (batch, seq_len, num_blocks, block_dim, block_dim)
+    num_blocks, block_dim = a.shape[-3], a.shape[-1]
+    assert (
+        num_blocks * block_dim == x.shape[-1]
+    ), f"({a.shape[-3]=} * {a.shape[-1]=})={num_blocks * block_dim} in a must match {x.shape[-1]=}"
+    x = rearrange(x, "... (n d) -> ... n d", n=num_blocks)  # (batch, seq_len, num_blocks, block_dim)
+
+    h_t = torch.zeros_like(x[:, 0])  # h_t: (batch, num_blocks, block_dim)
+    for t in range(x.shape[1]):
+        h_t = torch.einsum("... d k, ... k -> ... d", a[:, t], h_t) + x[:, t]
+    h_t = rearrange(h_t, "... n d -> ... (n d)", n=num_blocks)
+
     return h_t
---
+++ linear_rnn/triton/sequential_scan_block_diag_a.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def _sequential_scan_block_diag_a_fwd_kernel(
+    x_ptr,
+    a_ptr,
+    BLOCK_SIZE: tl.constexpr,
+):
+    """Sequential scan (with block-diagonal A) forward kernel, process BLOCK_SIZE_DIM elements."""
+    pass
+
+
+@triton.jit
+def _sequential_scan_block_diag_a_bwd_kernel():
+    """Sequential scan (with block-diagonal A) backward kernel, process BLOCK_SIZE_DIM elements."""
+    pass
+
+
+class SequentialScanBlockDiagA(torch.autograd.Function):
+    """Sequential scan (with block-diagonal A), parallelized across "dim" dimension."""
+
+    @staticmethod
+    @torch.cuda.amp.custom_fwd
+    def forward(ctx: Any, x: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @staticmethod
+    @torch.cuda.amp.custom_bwd
+    def backward(ctx: Any, *grad_outputs: Any) -> Any:
+        pass
